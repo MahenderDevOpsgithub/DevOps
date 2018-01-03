0f3fce5e51fddda0f3e78dee2842f769de84a41d
MahenderDevOpsGit <mahender.p612@gmail.com>
2018-01-03 13:42:48 +0530

    ssh-ifcondition

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,14 +36,14 @@ File { backup => 'main' }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-festsnode default {
+node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
 include apache
 }
 node 'server2.abc.com' {
-include apache
-include motdsample
+#include apache
+#include motdsample
+include ssh
 }
-#include motd
